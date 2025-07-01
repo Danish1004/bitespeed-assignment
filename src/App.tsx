9740c96965553e45df30c3f17ea6159eab3dcb53
Danish Sharma <danishsharma13255@gmail.com>
2025-07-01 22:23:39 +0530

    Initialize React project.Set up basic app structure with App component and index files.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useCallback, useState } from "react";
+
+// Register custom node type
+
+function App() {
+  
+  return (
+    <div className="flex h-screen w-screen">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
